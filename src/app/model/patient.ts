6cbd7e4b80e43b0172d064ec7237a583daf34672
mahakatas <mahakatas@gmail.com>
2023-07-14 17:59:30 +0200

    added logic for patient data

--- src/app/model/patient.ts
+++ src/app/model/patient.ts
@@ -1,2 +1,17 @@
 export class Patient {
+
+  id: number = 0;
+
+ patientNumber: string = "";
+
+ firstName: string = "";
+
+ surname: string = "";
+
+  address: string="";
+
+  dateOfBirth: string = "";
+
+  gender = "";
+
 }
--- src/app/service/patient.service.ts
+++ src/app/service/patient.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Patient } from '../model/patient';
+import { Observable } from 'rxjs';
+import {Router} from "@angular/router";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PatientService {
 
-  constructor() { }
+  private patientsUrl: string;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.patientsUrl = 'http://localhost:8080/screening/';
+  }
+
+  public findAll(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(this.patientsUrl+"patients");
+  }
+
+  public save(Patient: Patient) {
+    return this.http.post<Patient>(this.patientsUrl+"addPatient", Patient);
+  }
+
+  public delete(Patientname: String) {
+
+    return this.http.post<Patient>(this.patientsUrl+"deletePatient" , Patientname);
+  }
+
+  gotoPatientList() {
+    this.router.navigate(['/patients']);
+  }
 }
