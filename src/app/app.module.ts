525b32e5d9884293522f66d958137662a789eddc
mahakatas <mahakatas@gmail.com>
2023-07-14 16:59:58 +0200

    initialised components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PatientComponent } from './patient/patient.component';
+import { TreatmentComponent } from './treatment/treatment.component';
+import { PatientWeightComponent } from './patient-weight/patient-weight.component';
+import { PatientHeightComponent } from './patient-height/patient-height.component';
+import { PatientBPComponent } from './patient-bp/patient-bp.component';
+import { PatientBGComponent } from './patient-bg/patient-bg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PatientComponent,
+    TreatmentComponent,
+    PatientWeightComponent,
+    PatientHeightComponent,
+    PatientBPComponent,
+    PatientBGComponent
   ],
   imports: [
     BrowserModule
