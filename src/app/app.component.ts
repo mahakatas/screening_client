8f3ee2fc3383711d794f036c5cf356235c0643dd
mahakatas <mahakatas@gmail.com>
2023-07-14 16:55:35 +0200

    added initial screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'screeningClient';
+
+  title: string;
+
+  constructor() {
+    this.title = 'Diabetic Screening';
+  }
 }
